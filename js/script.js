847229fe7350f6e60386daeafce60524c09b51d7
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-07 18:34:41 -0700

    add validation for empty field

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//   - Any field is empty when the form is submitted
+//   - The day number is not between 1-31
+//   - The month number is not between 1-12
+//   - The date is in the future
+//   - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+// .error-input {
+//   border: 1px solid var(--light-red);
+// }
+
+// .error-text {
+//   color: var(--light-red);
+// }
+
+// .error-span {
+//   font-size: 0.9rem;
+// }
+
+// .error-hide {
+//   display: none;
+// }
+
+const sumbitBtn = document.querySelector("button");
+const dateInputDivs = document.querySelectorAll(".date-con");
+
+const checkIfEmpty = (dateInputDiv) => {
+  const input = dateInputDiv.querySelector("input");
+  const label = dateInputDiv.querySelector("label");
+  const span = dateInputDiv.querySelector("span");
+
+  //   console.log(input.validity);
+  input.classList.toggle("error-input", input.validity.valueMissing);
+  label.classList.toggle("error-text", input.validity.valueMissing);
+  span.classList.toggle("error-text", input.validity.valueMissing);
+  span.classList.toggle("error-hide", !input.validity.valueMissing);
+  span.textContent = "This field is required";
+};
+sumbitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  for (const input of dateInputDivs) {
+    checkIfEmpty(input);
+  }
+});
